3243f467116fca07408c13bed2cb7754ab2829f5
sjpollard <samjosephpollard@gmail.com>
2022-02-17 20:20:19 +0000

    Messing about with alpha

--- grouptesting.py
+++ grouptesting.py
@@ -1,4 +1,4 @@
-from cProfile import label
+import math
 import numpy as np
 from matplotlib import pyplot as plt
 rng = np.random.default_rng()
@@ -37,6 +37,8 @@ def scomp_decoder(test_matrix, test_outcomes):
         np.where(test_matrix[unexplained_tests].T[np.delete(pd_indices, ignored_indices)][most_unexplained] == 1)[0])
     return np.sort(scomp_indices) + 1
 
+#input n, T and k
+#output test matrix, test outcomes and defective set according to Bernoulli design
 def test_generator(num_items, num_tests, num_defect):
     prob = 1.0 / num_defect
     test_defectives = np.sort(rng.choice(np.arange(1, num_items + 1), num_defect, replace=False))
@@ -46,6 +48,8 @@ def test_generator(num_items, num_tests, num_defect):
     test_outcomes = np.array([np.where(np.sum(test_matrix * defectivity_vector, axis=1) > 0, 1, 0)])
     return test_matrix, test_outcomes.T, test_defectives
 
+#input n, T and k along with number of samples
+#output empirical probability of success over these samples
 def empirical_rate(num_items, num_tests, num_defect, size):
     correct_tests = np.array([[0, 0, 0]])
     for i in range(size):
@@ -55,16 +59,26 @@ def empirical_rate(num_items, num_tests, num_defect, size):
         correct_tests[0][2] += np.array_equal(scomp_decoder(sample[0], sample[1]), sample[2])
     return(correct_tests/float(size))
 
+#input fixed n and k
+#output list of empirical rates as T varies
 def vary_tests(num_items, num_defect, size):
-    results = np.zeros((size, 3))
+    results = np.zeros((num_items, 3))
     for i in range(1, num_items + 1):
-        results[i - 1] = empirical_rate(100, i, num_defect, size)
+        results[i - 1] = empirical_rate(num_items, i, num_defect, size)
     return results.T
 
+#input fixed n and T
+#output list of empirical rates as k varies
 def vary_defects(num_items, num_tests, size):
-    results = np.zeros((size, 3))
+    results = np.zeros((num_items, 3))
     for i in range(1, num_items + 1):
-        results[i - 1] = empirical_rate(100, num_tests, i, size)
+        results[i - 1] = empirical_rate(num_items, num_tests, i, size)
+    return results.T
+
+def vary_alpha(num_items, num_tests, size):
+    results = np.zeros((100, 3))
+    for i in range(0, 100):
+        results[i] = empirical_rate(num_items, num_tests, math.ceil(math.pow(num_items, i/float(100))), size)
     return results.T
 
 def plot_results_tests(results):
@@ -85,6 +99,14 @@ def plot_results_defects(results):
     plt.ylabel("success probability")
     plt.show()
 
+def plot_results_alpha(results):
+    plt.scatter(np.arange(0, 1, 0.01), results[0], c="b", marker="x", label="COMP")
+    plt.scatter(np.arange(0, 1, 0.01), results[1], c="r", marker="x", label="DD")
+    plt.scatter(np.arange(0, 1, 0.01), results[2], c="g", marker="x", label="SCOMP")
+    plt.legend(loc='upper right')
+    plt.xlabel("number of defectives")
+    plt.ylabel("success probability")
+    plt.show()
 
 def main():
     test_matrix = np.array([[1, 1, 1, 1, 0, 0, 0, 0], 
@@ -105,9 +127,9 @@ def main():
                             [0, 1, 0, 0, 0, 1, 1, 1],
                             [0, 0, 1, 0, 0, 1, 0, 1]])
     test_outcomes3 = np.array([[1], [1], [0], [1], [0]])   
-    plot_results_tests(vary_tests(100, 5, 100))
-    plot_results_defects(vary_defects(100, 60, 100))
-    
+    #plot_results_tests(vary_tests(100, 5, 100))
+    #plot_results_defects(vary_defects(100, 60, 100))
+    #plot_results_alpha(vary_alpha(100, 60, 100))    
     
 if __name__ == "__main__":
     main()
